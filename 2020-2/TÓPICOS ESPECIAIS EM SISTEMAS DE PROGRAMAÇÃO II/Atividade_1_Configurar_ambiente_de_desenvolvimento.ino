aa3a59a0d28d643047722f57fcf87f30d48058c4
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-02-06 00:15:18 -0300

    Fazndo atividade 2

--- 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Atividade_1_Configurar_ambiente_de_desenvolvimento.ino
+++ 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Atividade_1_Configurar_ambiente_de_desenvolvimento.ino
@@ -1,10 +1,12 @@
-void setup() {
-  pinMode(LED_BUILTIN,OUTPUT);
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void loop() {
-  digitalWrite(LED_BUILTIN,HIGH);
+void loop()
+{
+  digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
-  digitalWrite(LED_BUILTIN,LOW);
-  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20);
 }
