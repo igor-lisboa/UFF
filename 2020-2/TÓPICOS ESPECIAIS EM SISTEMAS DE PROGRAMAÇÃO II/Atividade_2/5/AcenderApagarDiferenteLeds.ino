c5f8d895ba992c690589c0723f0f3c618cf1c0fb
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-02-09 13:49:08 -0300

    Realizando atividade 5.2

--- 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Atividade_2/5/AcenderApagarDiferenteLeds.ino
+++ 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Atividade_2/5/AcenderApagarDiferenteLeds.ino
@@ -1,35 +1,16 @@
-#include "Thread.h"   // usando a biblioteca https://github.com/ivanseidel/ArduinoThread
-#include "ThreadController.h"
-
-#define LED_EXTERNO_SLEEP 2000
 #define LED_EXTERNO_PIN 11
 
-#define LED_INTERNO_SLEEP 1000
+int ledExternoAceso=0;
+int ledInternoAceso=0;
+int ciclosPassados=0;
 
-void acendeApagaLed(uint8_t pin, unsigned long millisecs, int aceso = 1)
+int toogleLed(uint8_t pin, int aceso = 1)
 {
-    //Acende o led no pin
+    aceso=(aceso == 1 ? 0 : 1);
+    //se led tiver aceso => apaga | se tiver apagado => acendw
     digitalWrite(pin, (aceso == 1 ? HIGH : LOW));
-
-    //Aguarda intervalo de tempo em milissegundos
-    delay(millisecs);
-
-    //Apaga o led no pin
-    digitalWrite(pin, (aceso == 1 ? LOW : HIGH));
+    return aceso;
 }
-
-void piscaLedExterno(){
-   acendeApagaLed(LED_EXTERNO_PIN,LED_EXTERNO_SLEEP);
-}
-
-void piscaLedInterno(){
-  acendeApagaLed(LED_BUILTIN,LED_INTERNO_SLEEP);
-}
-
-ThreadController cpu;
-
-Thread threadLedExterno;
-Thread threadLedInterno;
 
 void setup() {
      //Define a porta do led 1 como saida
@@ -37,17 +18,21 @@ void setup() {
 
     //Define a porta do led builtin como saida
     pinMode(LED_BUILTIN, OUTPUT);
-
-    threadLedExterno.setInterval(LED_EXTERNO_SLEEP);
-    threadLedExterno.onRun(piscaLedExterno);
-
-    threadLedInterno.setInterval(LED_INTERNO_SLEEP);
-    threadLedInterno.onRun(piscaLedInterno);
-
-    cpu.add(&threadLedExterno);
-    cpu.add(&threadLedInterno);
 }
 
 void loop() {
-  cpu.run();
+  //muda estado do led interno
+  ledInternoAceso=toogleLed(LED_BUILTIN, ledInternoAceso);
+
+  //p led externo tem q aguardar 2 segundos p fazer a alteracao
+  if(ciclosPassados==2){
+      ledExternoAceso=toogleLed(LED_EXTERNO_PIN, ledExternoAceso);
+      ciclosPassados=0;
+  }
+
+  //adiciona mais um ciclo passado
+  ciclosPassados++;
+  
+  //faz o ciclo de 1 segundo
+  delay(1000);
 }
