18236991309b5ae88f19328ad03e9bdd9502caf3
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-02-15 13:52:42 -0300

    Ajustando atividade 1 de topcs esp em sis prog p rodar no esp32

--- 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Atividade_1/Atividade_1.ino
+++ 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Atividade_1/Atividade_1.ino
@@ -1,3 +1,4 @@
+int LED_BUILTIN = 2;
 void setup()
 {
   pinMode(LED_BUILTIN, OUTPUT);
@@ -8,5 +9,5 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
   delay(500);
   digitalWrite(LED_BUILTIN, LOW);
-  delay(20);
+  delay(1000);
 }
