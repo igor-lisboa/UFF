a142348e0ac44529f9038b2dd0c4a112425ca0b8
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-04-23 16:29:49 -0300

    INiciando trabalho final de IOT

---
+++ 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Trabalho Final/getToken.sh
@@ -0,0 +1,1 @@
+curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"username":"igor.lisboa@icraft.com.br", "password":"guerr@EhBrab0"}' 'https://demo.thingsboard.io/api/auth/login' > tokenFile.json
