0a50d7a42d86938a0e91c0090beac959a9c6eee9
Igor Lisboa <igor.rodrigues.lisboa@gmail.com>
2021-05-04 01:35:32 -0300

    tirando arqs grandes do repo q estao no google drive e tirando itens q n serao mais usados

--- 2020-2/TÓPICOS ESPECIAIS EM SISTEMAS DE PROGRAMAÇÃO II/Trabalho Final/getToken.sh
+++
@@ -1,1 +0,0 @@
-curl -X POST --header 'Content-Type: application/json' --header 'Accept: application/json' -d '{"username":"igor.lisboa@icraft.com.br", "password":"guerr@EhBrab0"}' 'https://demo.thingsboard.io/api/auth/login' > tokenFile.json
